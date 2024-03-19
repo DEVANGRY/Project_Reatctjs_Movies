26ac11a13d89630d765fd7a612d6ebb4df3524fb
QTuan <tuanlqhe173471@fpt.edu.vn>
2024-03-19 20:40:04 +0700

    call api movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 import "./App.css";
 
 function App() {
+    useEffect(() => {
+        apiTesting();
+    }, []);
+    const apiTesting = () => {
+        fetchDataFromApi(`/movie/popular`).then((res) => {
+            console.log(res);
+        });
+    };
     return <div>App</div>;
 }
 
