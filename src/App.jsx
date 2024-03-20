0c21951aa578161ed8bdb9e668e828c501e01ef7
QTuan <tuanlqhe173471@fpt.edu.vn>
2024-03-20 09:58:58 +0700

    redux store call api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import "./App.css";
-
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 function App() {
+    const dispatch = useDispatch();
+    const { url } = useSelector((state) => state.home);
     useEffect(() => {
         apiTesting();
     }, []);
     const apiTesting = () => {
         fetchDataFromApi(`/movie/popular`).then((res) => {
-            console.log(res);
+            dispatch(getApiConfiguration(res));
         });
     };
-    return <div>App</div>;
+    return (
+        <div>
+            App
+            {url?.total_pages}
+        </div>
+    );
 }
 
 export default App;
