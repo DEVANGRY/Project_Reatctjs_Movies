75d62ba12e8c99f35d2bee78ec05697293f8f6dc
QTuan <tuanlqhe173471@fpt.edu.vn>
2024-03-21 01:11:03 +0700

    feature search UI

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { HiOutlineSearchCircle } from "react-icons/hi";
+import { HiOutlineSearch } from "react-icons/hi";
 import { SlMenu } from "react-icons/sl";
 import { VscChromeClose } from "react-icons/vsc";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -14,8 +14,24 @@ const Header = () => {
     const [showSearch, setShowSearch] = useState("");
     const navigate = useNavigate();
     const location = useLocation();
+    const openSearch = () => {
+        setMobileMenu(false);
+        setShowSearch(!showSearch);
+    };
+    const openMenuMobile = () => {
+        setMobileMenu(true);
+        setShowSearch(false);
+    };
+    const searchQueryHandle = (e) => {
+        if (e.key === "Enter" && query.length > 0) {
+            navigate(`/search/${query}`);
+            setInterval(() => {
+                setShowSearch(false);
+            }, 1000);
+        }
+    };
     return (
-        <header className="header">
+        <header className={`header ${show} ${mobileMenu ? "mobileView" : ""}`}>
             <ContentWrapper>
                 <div className="logo">
                     <img src={logo} alt="Logo Details" />
@@ -24,14 +40,37 @@ const Header = () => {
                     <li className="item">Movies</li>
                     <li className="item">TV Shows</li>
                     <li className="item">
-                        <HiOutlineSearchCircle />
+                        <HiOutlineSearch onClick={openSearch} />
                     </li>
                 </ul>
                 <div className="mobileMenuItems">
-                    <HiOutlineSearchCircle />
-                    {mobileMenu ? <VscChromeClose /> : <SlMenu />}
+                    <HiOutlineSearch onClick={openSearch} />
+                    {mobileMenu ? (
+                        <VscChromeClose onClick={() => setMobileMenu(false)} />
+                    ) : (
+                        <SlMenu onClick={openMenuMobile} />
+                    )}
                 </div>
             </ContentWrapper>
+            {showSearch && (
+                <div className="searchBar">
+                    <ContentWrapper>
+                        <div className="searchInput">
+                            <input
+                                type="text"
+                                placeholder="Search for a movies or tv show ..."
+                                onChange={(e) => {
+                                    setQuery(e.target.value);
+                                }}
+                                onKeyUp={searchQueryHandle}
+                            />
+                            <VscChromeClose
+                                onClick={() => setShowSearch(false)}
+                            />
+                        </div>
+                    </ContentWrapper>
+                </div>
+            )}
         </header>
     );
 };
