78b9cf1166313f041886425ddd3560ddd226aace
QTuan <tuanlqhe173471@fpt.edu.vn>
2024-03-22 10:08:12 +0700

    create rating movies

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -12,12 +12,23 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 // import CircleRating from "../circleRating/CircleRating";
 // import Genres from "../genres/Genres";
-
+import CircleRating from "../circaleRating/CircleRating";
 const Carousel = ({ data, loading }) => {
     const carouselContainer = useRef();
     const { url } = useSelector((state) => state.home);
     const navigate = useNavigate();
     const navigation = () => {};
+    const skItem = () => {
+        return (
+            <div className="skeletonItem">
+                <div className="posterBlock skeleton"></div>
+                <div className="textBlock">
+                    <span className="title skeleton"></span>
+                    <span className="date skeleton"></span>
+                </div>
+            </div>
+        );
+    };
     return (
         <div className="carousel">
             <ContentWrapper>
@@ -41,13 +52,34 @@ const Carousel = ({ data, loading }) => {
                                 <div className="carouselItem" key={item.id}>
                                     <div className="posterBlock">
                                         <Img src={posterUrl} />
+                                        <CircleRating
+                                            rating={item.vote_average.toFixed(
+                                                1
+                                            )}
+                                        />
+                                    </div>
+                                    <div className="textBlock">
+                                        <span className="title">
+                                            {item.name || item.title}
+                                        </span>
+                                        <span className="date">
+                                            {dayjs(item.first_air_date).format(
+                                                "MMM D YYYY"
+                                            )}
+                                        </span>
                                     </div>
                                 </div>
                             );
                         })}
                     </div>
                 ) : (
-                    <span>Loading ...</span>
+                    <div className="loadingSkeleton">
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                        {skItem()}
+                    </div>
                 )}
             </ContentWrapper>
         </div>
